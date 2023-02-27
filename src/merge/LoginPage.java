d072502f88e638ba93e9358e2bdf7cbd96ce5204
Ahmet <ahmetyilmaznnb2005@gmail.com>
2023-02-27 19:22:55 +0100

    Login Test uplodet

---
+++ src/merge/LoginPage.java
@@ -0,0 +1,6 @@
+package merge;
+
+public class LoginPage {
+    // Test 1 tamamlandi
+
+}
--- src/newtask/Test1.java
+++ src/newtask/Test1.java
@@ -1,7 +1,7 @@
 package newtask;
 
 public class Test1 {
-
+// haloo
     //Ide de degisiklik yapildi
     // Konsoldan degil soldan commitleme denendo
     // bu kisim remote da olusturuldu
